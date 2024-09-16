830a8e6784aeb53933eabb4c671bb0eb0de88fbf
Artem <a.v.lyamichev@gmail.com>
2024-09-16 16:21:45 -0300

    feat: tailiwind configuration

---
+++ frontend-migration/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Включает все файлы в папке src с расширениями js, ts, jsx, tsx
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

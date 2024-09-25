65ef3eb0b23b1e86c527a06c4ad40b0f67f4c4f3
Artem <a.v.lyamichev@gmail.com>
2024-09-25 12:09:54 -0300

    feat: react-router-dom config

--- frontend-migration/src/main.tsx
+++ frontend-migration/src/main.tsx
@@ -1,10 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Contact from "./routes/contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );

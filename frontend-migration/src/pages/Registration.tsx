6cc7acf2bd6e80869efc83c2d9ac8cc827a28d7f
Artem <a.v.lyamichev@gmail.com>
2024-10-03 14:57:54 -0300

    feat: Registration validation

--- frontend-migration/src/pages/Registration.tsx
+++ frontend-migration/src/pages/Registration.tsx
@@ -1,37 +1,66 @@
 import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { Link } from "react-router-dom";
-import clsx from "clsx";
-import { buttonVariants } from "@/components/ui/button";
-import { UserAuthForm } from "@/components/user-auth-form";
 import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { PasswordInput } from "@/components/ui/password-input";
-import { useNavigate } from "react-router-dom";
-import logo from "../assets/rarus-logo-vertical.png";
 import { LanguageSwitcher } from "@/components/ui/language-switcher.tsx";
 import "../styles/modal.css"; // Import your custom styles
+import axios from "axios";
+import regexPassword from "../utils/constants.ts";
+const registerUser = async (data: {
+  email: string;
+  password: string;
+  confirmedPassword: string;
+}) => {
+  const response = await axios.post(
+    "https://rarus-health-qa.uc.r.appspot.com/users",
+    data
+  );
+  return response.data;
+};
 
-interface VideoModalProps {
-  getVideoUrl: () => string; // Type for the function
-}
+type FormInputs = {
+  email: string;
+  password: string;
+};
+
 export default function AuthenticationPage() {
-  const navigate = useNavigate();
-  const [currentPassword, setCurrentPassword] = useState("");
-  const handleRegistration = () => {
-    // Here you would typically add your login logic
-    // For now, let's just navigate to the MainPage on button click
-    console.log("handling registration...");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<FormInputs>({
+    mode: "onChange",
+  });
+  console.log("errors:", errors.email);
+  const [termsError, setTermsError] = useState(false);
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    const { email, password } = data;
+    if (termsAccepted && policyAccepted) {
+      setTermsError(false);
+      registerUser({
+        email: email,
+        password: password,
+        confirmedPassword: password,
+        //12345aA!a
+      });
+    } else {
+      setTermsError(true);
+    }
   };
+
+  const [policyAccepted, setPolicyAccepted] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
+  const handleRegistration = () => {};
   const videoLink =
     "https://www.youtube.com/embed/JiUQ9pIUObQ?si=Dr3K3dZ_v8ANfN_P";
   return (
@@ -44,140 +73,146 @@ export default function AuthenticationPage() {
           <div className="modal flex flex-col justify-between w-full">
             <iframe
               src={videoLink}
-              className="w-full h-full video-desktop"
-              style={{ padding: "16px", borderRadius: "10px" }}
+              className="w-full h-full video-desktop p-4"
               title="Video"
             />
           </div>{" "}
         </div>
 
-        <Card className="min-w-[312px]">
-          <CardHeader className="space-y-1">
-            <CardTitle className="text-lg text-center">
-              Registro de usuario
-            </CardTitle>
-          </CardHeader>
-          <CardContent className="grid gap-4">
-            <div className="grid gap-2">
-              <Input id="email" type="email" placeholder="Email" />
-            </div>
-            <div className="grid gap-2">
-              <PasswordInput
-                id="current_password"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                autoComplete="current-password"
-              />
-            </div>
-            <div>
-              <div className="text-left mt-4 flex items-center">
-                <input
-                  className="mr-1 h-5 w-5"
-                  type="checkbox"
-                  id="agree"
-                  name="agree"
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Card className="min-w-[312px]">
+            <CardHeader className="space-y-1">
+              <CardTitle className="text-lg text-center">
+                Registro de usuario
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "Invalid email address",
+                    },
+                  })}
                 />
-                <label htmlFor="agree">
-                  He leído y acepto{" "}
-                  <a
-                    href="https://s3.rarus.health/rarus/Politicas_de_Privacidad_Rarus_Health.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="underline font-normal text-gray-800"
-                  >
-                    la Política de privacidad
-                  </a>
-                </label>
+                {errors.email && (
+                  <span className="text-red-500">
+                    {errors.email.message || "Error"}
+                  </span>
+                )}{" "}
               </div>
-              <div className="text-left mt-2 flex items-center">
-                <input
-                  className="mr-1 h-5 w-5"
-                  type="checkbox"
-                  id="agree2"
-                  name="agree2"
-                />
-                <label htmlFor="agree2">
-                  He leído y acepto{" "}
-                  <a
-                    href="https://s3.rarus.health/rarus/Condiciones-de-servicio.pdf"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="underline font-normal text-gray-800"
-                  >
-                    las Condiciones del servicio
-                  </a>
-                </label>
+              <div className="grid gap-2">
+                <PasswordInput
+                  id="current_password"
+                  autoComplete="current-password"
+                  required
+                  {...register("password", {
+                    required: "Password is required",
+                    minLength: {
+                      value: 8,
+                      message: "Password must be at least 8 characters",
+                    },
+                    validate: {
+                      uppercase: (value) =>
+                        /[A-Z]/.test(value) ||
+                        "Password must contain at least one uppercase letter",
+                      lowercase: (value) =>
+                        /[a-z]/.test(value) ||
+                        "Password must contain at least one lowercase letter",
+                      symbol: (value) =>
+                        /[^A-Za-z0-9]/.test(value) ||
+                        "Password must contain at least one special character",
+                    },
+                  })}
+                />{" "}
+                {errors.password && (
+                  <span className="text-red-500">
+                    {errors.password.message || "Error"}
+                  </span>
+                )}{" "}
               </div>
-            </div>
-          </CardContent>
-          <CardFooter className="flex flex-col">
-            <Button
-              className="w-full"
-              onClick={handleRegistration}
-              type="submit"
-            >
-              Inscribirse
-            </Button>
-            <div className="mt-4 text-sm text-center ">
-              <Link to="/login" className="text-[#77858C] ">
-                <p className=" mt-4 hover:underline">Accesar</p>
-              </Link>
-            </div>
-          </CardFooter>
-        </Card>
+              <div>
+                <div className="text-left mt-4 flex items-center">
+                  <input
+                    className="mr-1 h-5 w-5"
+                    type="checkbox"
+                    id="agree"
+                    name="agree"
+                    checked={policyAccepted}
+                    onChange={() => setPolicyAccepted((prev) => !prev)}
+                  />
+                  <label htmlFor="agree">
+                    He leído y acepto{" "}
+                    <a
+                      href="https://s3.rarus.health/rarus/Politicas_de_Privacidad_Rarus_Health.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline font-normal text-gray-800"
+                    >
+                      la Política de privacidad
+                    </a>
+                  </label>
+                </div>
+                <div className="text-left mt-2 flex items-center">
+                  <input
+                    className="mr-1 h-5 w-5"
+                    type="checkbox"
+                    id="agree2"
+                    name="agree2"
+                    checked={termsAccepted}
+                    onChange={() => setTermsAccepted((prev) => !prev)}
+                  />
+                  <label htmlFor="agree2">
+                    He leído y acepto{" "}
+                    <a
+                      href="https://s3.rarus.health/rarus/Condiciones-de-servicio.pdf"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="underline font-normal text-gray-800"
+                    >
+                      las Condiciones del servicio
+                    </a>
+                  </label>
+                </div>
+              </div>
+            </CardContent>
+            {termsError && (
+              <span className="text-red-500">
+                You must accept terms and policy before registering.
+              </span>
+            )}{" "}
+            <CardFooter className="flex flex-col">
+              <Button
+                className="w-full"
+                onClick={handleRegistration}
+                type="submit"
+              >
+                Inscribirse
+              </Button>
+              <div className="mt-4 text-sm text-center ">
+                <Link to="/login" className="text-[#77858C] ">
+                  <p className=" mt-4 hover:underline">Accesar</p>
+                </Link>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
+
         <div className="hidden md:flex max-w-lg w-full mt-10 md:m-0">
           <div className="modal-desktop flex flex-col justify-between w-full">
             <iframe
               src={videoLink}
-              className="w-full h-full video-desktop"
-              style={{ borderRadius: "10px" }} // Inline styles can also be applied
+              className="w-full h-full video-desktop border rounded-xl"
               title="Video"
             />
           </div>
         </div>
       </div>
     </div>
-    /*     <>
-      <div className="container relative  h-[800px] flex-col items-center justify-center ">
-        <div className="lg:p-8">
-          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-            <div className="flex flex-col space-y-2 text-center">
-              <h1 className="text-2xl font-semibold tracking-tight">
-                Create an account
-              </h1>
-              <p className="text-sm text-muted-foreground">
-                Enter your email below to create your account
-              </p>
-            </div>
-            <UserAuthForm />
-            <Link
-              to="/login"
-              className={clsx(
-                buttonVariants({ variant: "ghost" }) // Assuming this is defined elsewhere
-              )}
-            >
-              Login
-            </Link>
-            <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
-              <Link
-                to="https://twitter.com/shadcn"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                to="https://twitter.com/shadcn"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
-              </Link>
-              .
-            </p>
-          </div>
-        </div>
-      </div>
-    </> */
   );
 }
